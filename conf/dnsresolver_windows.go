5e4f8ef89487b0a0b72a37559d68fb26d94119ec
izumi46 <46021271+izumi46@users.noreply.github.com>
2023-10-22 18:52:36 +0800

    add srv support

--- conf/dnsresolver_windows.go
+++ conf/dnsresolver_windows.go
@@ -7,6 +7,7 @@ package conf
 
 import (
 	"log"
+	"net"
 	"net/netip"
 	"time"
 	"unsafe"
@@ -82,13 +83,59 @@ func resolveHostnameOnce(name string) (resolvedIPString string, err error) {
 	return
 }
 
+func resolveHostnameSRV(name string) (resolvedIPString string, resolvedPort uint16, err error) {
+	maxTries := 10
+	if services.StartedAtBoot() {
+		maxTries *= 3
+	}
+	for i := 0; i < maxTries; i++ {
+		if i > 0 {
+			time.Sleep(time.Second * 4)
+		}
+		resolvedIPString, resolvedPort, err = resolveHostnameSRVOnce(name)
+		if err == nil {
+			return
+		}
+		if err == windows.WSATRY_AGAIN {
+			log.Printf("Temporary DNS error when resolving %s, so sleeping for 4 seconds", name)
+			continue
+		}
+		if err == windows.WSAHOST_NOT_FOUND && services.StartedAtBoot() {
+			log.Printf("Host not found when resolving %s at boot time, so sleeping for 4 seconds", name)
+			continue
+		}
+		return
+	}
+	return
+}
+
+func resolveHostnameSRVOnce(name string) (resolvedIPString string, resolvedPort uint16, err error) {
+	_, srvs, err := net.LookupSRV("wg", "udp", name)
+	if err != nil {
+		return name, 0, err
+	}
+	if len(srvs) > 0 {
+		var target = srvs[0].Target
+		resolvedPort = srvs[0].Port
+		resolvedIPString, err = resolveHostnameOnce(target)
+		return
+	} else {
+		err = windows.WSAHOST_NOT_FOUND
+		return name, 0, err
+	}
+}
+
 func (config *Config) ResolveEndpoints() error {
 	for i := range config.Peers {
 		if config.Peers[i].Endpoint.IsEmpty() {
 			continue
 		}
 		var err error
-		config.Peers[i].Endpoint.Host, err = resolveHostname(config.Peers[i].Endpoint.Host)
+		if config.Peers[i].Endpoint.Port == 0 {
+			config.Peers[i].Endpoint.Host, config.Peers[i].Endpoint.Port, err = resolveHostnameSRV(config.Peers[i].Endpoint.Host)
+		} else {
+			config.Peers[i].Endpoint.Host, err = resolveHostname(config.Peers[i].Endpoint.Host)
+		}
 		if err != nil {
 			return err
 		}
